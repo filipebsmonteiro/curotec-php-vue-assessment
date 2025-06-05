f48b126602165b8c25b17c5e89e752f3d97db202
Filipe Monteiro <filipebsmonteiro@gmail.com>
2025-06-05 17:44:21 -0300

    Remove Create|Edit Routes from Controllers and Catch all redirecting to Front

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('app'); });
+Route::get('/{pathMatch}', function () { return view('app'); })->where('pathMatch', ".*");
 
 
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('/tasks', TaskController::class);
+    Route::resource('/tasks', TaskController::class)
+        ->except('create', 'edit');
 
-    Route::resource('/categories', CategoryController::class);
+    Route::resource('/categories', CategoryController::class)
+        ->except('create', 'edit');
 })->prefix('api');
